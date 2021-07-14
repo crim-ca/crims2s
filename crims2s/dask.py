81287dc2e423475b91dd06d2b4c72bf53ab8c01e
CORPO\gierscjo <jordan.gierschendorf@crim.ca>
2021-07-14 11:18:20 -0400

    adding generic function to connect dask

--- crims2s/dask.py
+++ crims2s/dask.py
@@ -2,7 +2,9 @@
 
 import os
 import yaml
-
+import getpass
+import dask.distributed
+import dask_jobqueue
 import dask.config
 
 
@@ -15,3 +17,20 @@ def load_project_defaults():
     with open(fn) as f:
         project_config = yaml.safe_load(f)
         dask.config.update(dask.config.config, project_config, priority="new")
+
+        
+def start_dask(conda_env: str = "s2s", jobs: int = 2):
+    
+    username = getpass.getuser()
+    homedir = os.path.expanduser("~")
+    
+    print("Start Dask for", username, "with", conda_env, "conda environment")
+    
+    bash_profile: str = os.path.join(homedir, '.bash_profile')
+    print('bash_profile:', bash_profile)
+    
+    env_extrat = ['source ' + bash_profile, 'conda activate ' + conda_env]    
+    cluster = dask_jobqueue.SLURMCluster(env_extra=env_extrat)
+    cluster.scale(jobs=jobs)
+
+    return dask.distributed.Client(cluster)
