c94b141468aa2e0f300edff0a0fa2028d71690ec
David Landry <david.landry@crim.ca>
2021-09-15 14:07:27 -0400

    Reorg config into an experiment

--- crims2s/training/train.py
+++ crims2s/training/train.py
@@ -3,7 +3,7 @@ import hydra
 import logging
 import os
 import pytorch_lightning as pl
-import pytorch_lightning.callbacks as callbacks
+import pytorch_lightning.callbacks as pl_callbacks
 import pytorch_lightning.loggers as loggers
 import torch
 
@@ -13,7 +13,7 @@ from ..util import ECMWF_FORECASTS
 _logger = logging.getLogger(__name__)
 
 
-class ModelCheckpoint(callbacks.ModelCheckpoint):
+class ModelCheckpoint(pl_callbacks.ModelCheckpoint):
     def on_save_checkpoint(
         self, trainer: pl.Trainer, pl_module: pl.LightningModule, checkpoint
     ):
@@ -37,15 +37,14 @@ def make_optimizer(cfg, model):
         raise RuntimeError("Coud not interpret optimizer configuration.")
 
 
-@hydra.main(config_path="conf", config_name="config")
-def cli(cfg):
-    transform = hydra.utils.instantiate(cfg.transform)
+def make_datasets(dataset_cfg, transform_cfg):
+    transform = hydra.utils.instantiate(transform_cfg)
 
-    train_years = list(range(2000, cfg.validate_from))
-    val_years = list(range(cfg.validate_from, 2020))
+    train_years = list(range(2000, dataset_cfg.validate_from))
+    val_years = list(range(dataset_cfg.validate_from, 2020))
 
-    if cfg.index is not None:
-        month, day = ECMWF_FORECASTS[cfg.index]
+    if dataset_cfg.index is not None:
+        month, day = ECMWF_FORECASTS[dataset_cfg.index]
         label = f"{month:02}{day:02}.nc"
 
         _logger.info("Targetting monthday %s", label)
@@ -55,35 +54,38 @@ def cli(cfg):
 
     train_dataset = TransformedDataset(
         S2SDataset(
-            cfg.dataset_dir,
+            dataset_cfg.dataset_dir,
             years=train_years,
             name_filter=name_filter,
-            include_features=cfg.include_features,
+            include_features=dataset_cfg.include_features,
         ),
         transform,
     )
     val_dataset = TransformedDataset(
         S2SDataset(
-            cfg.dataset_dir,
+            dataset_cfg.dataset_dir,
             years=val_years,
             name_filter=name_filter,
-            include_features=cfg.include_features,
+            include_features=dataset_cfg.include_features,
         ),
         transform,
     )
+
+    return train_dataset, val_dataset
+
+
+def run_experiment(cfg, num_workers=4, lr_find=False):
+    train_dataset, val_dataset = make_datasets(cfg.dataset, cfg.transform)
 
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=None,
         batch_sampler=None,
-        num_workers=int(cfg.num_workers),
+        num_workers=num_workers,
         shuffle=True,
     )
     val_dataloader = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size=None,
-        batch_sampler=None,
-        num_workers=int(cfg.num_workers),
+        val_dataset, batch_size=None, batch_sampler=None, num_workers=num_workers,
     )
 
     model = hydra.utils.instantiate(cfg.model)
@@ -112,21 +114,27 @@ def cli(cfg):
     )
     mlflow.log_hyperparams(cfg)
 
-    early_stopping = callbacks.EarlyStopping(
-        monitor="val_loss", patience=cfg.early_stopping.patience
-    )
     checkpointer = ModelCheckpoint(monitor="val_loss")
-    lr_monitor = callbacks.LearningRateMonitor()
+
+    callbacks = [
+        checkpointer,
+        pl_callbacks.LearningRateMonitor(),
+    ]
+    if "early_stopping" in cfg:
+        early_stopping = pl_callbacks.EarlyStopping(
+            monitor="val_loss", patience=cfg.early_stopping.patience
+        )
+        callbacks.append(early_stopping)
 
     trainer = pl.Trainer(
         log_every_n_steps=1,
         logger=[tensorboard, mlflow],
-        callbacks=[early_stopping, checkpointer, lr_monitor],
+        callbacks=callbacks,
         default_root_dir="./lightning/",
         **cfg.trainer,
     )
 
-    if cfg.lr_find:
+    if lr_find:
         import matplotlib.pyplot as plt
 
         lr_finder = trainer.tuner.lr_find(
@@ -141,6 +149,13 @@ def cli(cfg):
         trainer.fit(lightning_module, train_dataloader, val_dataloader)
         best_score = float(checkpointer.best_model_score.cpu())
         mlflow.log_metrics({"val_loss_min": best_score})
+
+
+@hydra.main(config_path="conf", config_name="config")
+def cli(cfg):
+    run_experiment(
+        cfg.experiment, num_workers=int(cfg.num_workers), lr_find=cfg.lr_find
+    )
 
 
 if __name__ == "__main__":
